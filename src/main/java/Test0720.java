8f12bff9d554fa815da4437785e149b19e7a2bc5
ChangQi <ChangQi.163.com>
2020-11-27 19:57:23 +0800

    hot-fix commit

--- src/main/java/Test0720.java
+++ src/main/java/Test0720.java
@@ -2,5 +2,6 @@ public class Test0720 {
     public static void main(String[] args) {
         System.out.println("ChangQi Hello");
         System.out.println("常琪最帅");
+        System.out.println("王磊最帅");
     }
 }
